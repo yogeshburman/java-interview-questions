6539e571617e0885c42ec5302100208c5e0fda1f
Yogesh Burman <82220086+yogeshburman@users.noreply.github.com>
2022-03-01 12:40:49 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,19 @@
+// Swapping no.
+class Test {
+  public static void main (String[]args){
+    
+    int a=10;
+    int b=20;
+    int temp;
+    
+    System.out.println("Swapping no. a = " + a + ", b = "+ b);
+    
+    // Swapping 
+    temp=a;
+    a = b;
+    b= temp;
+    
+    System.out.println("Swapping no. a = " + a + ", b = "+ b);
+    
+  }
+}
