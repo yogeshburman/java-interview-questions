977aca5e90344e15683be3d775763477c371e135
Yogesh Burman <82220086+yogeshburman@users.noreply.github.com>
2022-03-02 14:07:25 +0530

    Add files via upload

---
+++ Test (10).java
@@ -0,0 +1,26 @@
+// Find Largest Of 3 Numbers
+
+class Test {
+  public static void main (String[]args){
+    
+    int a=11;
+    int b=2;
+    int c=3;
+    
+    System.out.println(" value of a="+a);
+    System.out.println(" value of b="+b);
+    System.out.println(" value of c="+c);
+    System.out.println();
+    System.out.println(" Checking largest no.");
+    
+     if(a>b && a>b){
+      System.out.println(" a= " + a + " is largest no");
+    }else if(b>a && b>c){
+      System.out.println(" b= " + b + " is largest no");
+    }else if(c>b && c>a){
+      System.out.println(" c= " + c + " is largest no");
+    }
+    
+    
+  }
+}
